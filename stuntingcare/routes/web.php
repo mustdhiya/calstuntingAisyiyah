704dd4f794978c32a1f6ad8f822c7e724b3ce829
nikamushi <autoruns18@gmail.com>
2026-07-06 20:00:16 +0800

    feat: migrate static HTML mockups to Laravel Blade templates and configure routing

--- stuntingcare/routes/web.php
+++ stuntingcare/routes/web.php
@@ -3,5 +3,56 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+})->name('home');
+
+Route::get('/tentang', function () {
+    return view('tentang');
+})->name('tentang');
+
+Route::get('/edukasi', function () {
+    return view('edukasi');
+})->name('edukasi');
+
+Route::get('/kalkulator', function () {
+    return view('kalkulator');
+})->name('kalkulator');
+
+Route::get('/hasil', function () {
+    return view('hasil');
+})->name('hasil');
+
+Route::get('/faq', function () {
+    return view('faq');
+})->name('faq');
+
+Route::get('/kontak', function () {
+    return view('kontak');
+})->name('kontak');
+
+Route::get('/artikel-detail', function () {
+    return view('artikel-detail');
+})->name('artikel.detail');
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', function () {
+        return view('admin.dashboard');
+    })->name('dashboard');
+
+    Route::get('/analisis', function () {
+        return view('admin.analisis');
+    })->name('analisis');
+
+    Route::get('/artikel', function () {
+        return view('admin.artikel-list');
+    })->name('artikel.list');
+
+    Route::get('/artikel/edit', function () {
+        return view('admin.artikel-edit');
+    })->name('artikel.edit');
+
+    Route::get('/pengguna', function () {
+        return view('admin.pengguna');
+    })->name('pengguna');
 });
+
